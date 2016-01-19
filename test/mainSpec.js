a57af6afb58ecf044210344fd6891d65b0e466ba
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2016-01-19 13:42:51 +0000

    tests finished.

--- test/mainSpec.js
+++ test/mainSpec.js
@@ -3,6 +3,115 @@
 var LB = require('os').EOL;
 var minify = require('../lib');
 var parser = require('../lib/parser');
+var errorLib = require('../lib/error');
+
+describe("Minify/Positive", function () {
+
+    describe("single-line comment", function () {
+        it("must return an empty string", function () {
+            expect(minify("--comment")).toBe("");
+            expect(minify("--comment" + LB)).toBe("");
+            expect(minify(LB + "--comment")).toBe("");
+        });
+    });
+
+    describe("single-line comment with a prefix", function () {
+        it("must return the prefix", function () {
+            expect(minify("text--comment")).toBe("text");
+            expect(minify(" text --comment")).toBe("text");
+            expect(minify("text" + LB + "--comment")).toBe("text");
+            expect(minify(" text " + LB + "--comment")).toBe("text");
+            expect(minify("text--comment" + LB)).toBe("text");
+        });
+    });
+
+    describe("single-line comment with a suffix", function () {
+        it("must return the suffix", function () {
+            expect(minify("--comment" + LB + "text")).toBe("text");
+            expect(minify("--comment" + LB + " text ")).toBe("text");
+        });
+    });
+
+    describe("comments in strings", function () {
+        it("must be skipped", function () {
+            expect(minify("'--comment'")).toBe("'--comment'");
+            expect(minify("'/*comment*/'")).toBe("'/*comment*/'");
+        });
+    });
+
+    describe("empty text", function () {
+        it("must be returned empty", function () {
+            expect(minify("")).toBe("");
+            expect(minify("''")).toBe("''");
+        });
+    });
+
+    describe("quotes in strings", function () {
+        it("must be ignored", function () {
+            expect(minify("''''")).toBe("''''");
+            expect(minify("''''''")).toBe("''''''");
+        });
+    });
+
+    describe("with multiple lines", function () {
+        it("must be ignored", function () {
+            expect(minify("--comment" + LB + LB + "text")).toBe("text");
+            expect(minify("/*start" + LB + "end*/")).toBe("");
+            expect(minify("/*start" + LB + "end*/text")).toBe("text");
+            expect(minify("start-/*comment*/end")).toBe("start-end");
+            expect(minify("start/*comment*/" + LB + "end")).toBe("start end");
+            expect(minify("start/*comment*/" + LB + " end")).toBe("start end");
+            expect(minify("/*comment*/end " + LB)).toBe("end");
+            expect(minify("/*start" + LB + "end*/" + LB + "text")).toBe("text");
+        });
+    });
+
+});
+
+describe("Minify/Negative", function () {
+
+    describe("quotes in strings", function () {
+        var errMsg = "Error parsing SQL at {line:1,col:1}: Unclosed text block.";
+        it("must throw an error", function () {
+            expect(function () {
+                minify("'");
+            }).toThrow(errMsg);
+
+            expect(function () {
+                minify("'''");
+            }).toThrow(errMsg);
+
+            expect(function () {
+                minify("'''");
+            }).toThrow(new errorLib.SQLParsingError(errorLib.parsingErrorCode.unclosedText, "Unclosed text block.", {
+                line: 1,
+                column: 1
+            }));
+
+            expect(function () {
+                minify("/*");
+            }).toThrow(new errorLib.SQLParsingError(errorLib.parsingErrorCode.unclosedMLC, "Unclosed multi-line comment.", {
+                line: 1,
+                column: 1
+            }));
+
+        });
+    });
+
+    describe("unclosed multi-lines", function () {
+        var errMsg = "Error parsing SQL at {line:1,col:1}: Unclosed multi-line comment.";
+        it("must throw an error", function () {
+            expect(function () {
+                minify("/*");
+            }).toThrow(errMsg);
+
+            expect(function () {
+                minify("/*text");
+            }).toThrow(errMsg);
+        });
+    });
+
+});
 
 describe("EOL", function () {
 
@@ -22,114 +131,31 @@ describe("EOL", function () {
     });
 });
 
-describe("Minify/Positive", function () {
-
-    describe("single-line comment", function () {
-        it("must return an empty string", function () {
-            expect(minify("--comment")).toBe("");
-        });
-    });
-
-    describe("single-line comment with a prefix", function () {
-        it("must return the prefix", function () {
-            expect(minify("text--comment")).toBe("text");
-        });
-    });
-
-    describe("comments in strings", function () {
-        it("must be skipped", function () {
-            expect(minify("'--comment'")).toBe("'--comment'");
-            expect(minify("'/*comment*/'")).toBe("'/*comment*/'");
-        });
-    });
-
-    describe("empty text", function () {
-        it("must be returned empty", function () {
-            expect(minify("")).toBe("");
-            expect(minify("''")).toBe("''");
-        });
-    });
-    describe("quotes in strings", function () {
-        it("must be ignored", function () {
-            expect(minify("''''")).toBe("''''");
-            expect(minify("''''''")).toBe("''''''");
-        });
-    });
-
-    describe("with multiple lines", function () {
-        it("must be ignored", function () {
-            expect(minify("--comment" + LB + "text")).toBe("text");
-            expect(minify("--comment" + LB + LB + "text")).toBe("text");
-            expect(minify("/*start" + LB + "end*/")).toBe("");
-            expect(minify("/*start" + LB + "end*/text")).toBe("text");
-            expect(minify("start-/*comment*/end")).toBe("start-end");
-            expect(minify("start/*comment*/" + LB + "end")).toBe("start end");
-
-            expect(minify("start/*comment*/" + LB + " end")).toBe("start end");
-
-            expect(minify("/*comment*/end " + LB)).toBe("end");
-        });
-    });
-
-});
-
-describe("Minify/Negative", function () {
-
-    describe("quotes in strings", function () {
-        var errMsg = "Error parsing SQL at {line:1,col:1}: Unclosed text block.";
-        it("must throw an error", function () {
-            expect(function () {
-                minify("'");
-            }).toThrow(errMsg);
-
-            expect(function () {
-                minify("'''");
-            }).toThrow(errMsg);
-
-            try {
-                minify("'''text");
-            } catch (e) {
-                //expect(e.inspect()).toBe(e.message + LB + LB + e.stack);
-            }
-        });
-    });
-
-    describe("unclosed multi-lines", function () {
-        var errMsg = "Error parsing SQL at {line:1,col:1}: Unclosed multi-line comment.";
-        it("must throw an error", function () {
-            expect(function () {
-                minify("/*");
-            }).toThrow(errMsg);
-
-            expect(function () {
-                minify("/*text");
-            }).toThrow(errMsg);
-        });
-    });
-
-});
-
 describe("Index Position:", function () {
 
     function pos(text, idx) {
         return parser.getIndexPos(text, idx, "\r\n");
     }
 
-    describe("", function () {
-        it("", function () {
-            expect(pos("123\r\n456", 0)).toEqual({
-                line: 1,
-                column: 1
-            });
-            expect(pos("123\r\n456", 5)).toEqual({
-                line: 2,
-                column: 1
-            });
-            expect(pos("123\r\n", 3)).toEqual({
-                line: 1,
-                column: 3
-            });
-
+    it("must work from the start", function () {
+        expect(pos("123\r\n456", 0)).toEqual({
+            line: 1,
+            column: 1
         });
     });
+
+    it("step over lines correctly", function () {
+        expect(pos("123\r\n456", 5)).toEqual({
+            line: 2,
+            column: 1
+        });
+    });
+
+    it("must step back from a line break", function () {
+        expect(pos("123\r\n", 3)).toEqual({
+            line: 1,
+            column: 3
+        });
+    });
+
 });
