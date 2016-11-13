2478e0a2a9aebbf1c9473d50255d0b4650388e55
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2016-11-13 15:56:23 +0000

    adding TypeScript support.

---
+++ typescript/index.d.ts
@@ -0,0 +1,35 @@
+//////////////////////////////////////////////////////////
+// pg-minify: https://github.com/vitaly-t/pg-minify
+//////////////////////////////////////////////////////////
+
+declare module 'pg-minify' {
+
+    interface IErrorPosition {
+        line:number,
+        column:number
+    }
+
+    namespace pgMinify {
+
+        export enum parsingErrorCode {
+            unclosedMLC = 1,    // Unclosed multi-line comment.
+            unclosedText = 2,   // Unclosed text block.
+            unclosedQI = 3,     // Unclosed quoted identifier.
+            multiLineQI = 4     // Multi-line quoted identifiers are not supported.
+        }
+
+        export class SQLParsingError implements Error {
+            name:string;
+            message:string;
+            stack:string;
+            error:string;
+            code:parsingErrorCode;
+            position:IErrorPosition;
+        }
+
+    }
+
+    function pgMinify(sql:string, options?:{compress?:boolean}):string;
+
+    export = pgMinify;
+}
