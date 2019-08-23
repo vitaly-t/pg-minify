99731bfe3fd042787a6fff2cafdd80cc5ed9f6ea
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2019-08-23 19:41:16 +0100

    fixing ts test

--- test/typescript/main.ts
+++ test/typescript/main.ts
@@ -4,5 +4,5 @@ const s1 = minify('select * from table; --comment');
 const s2 = minify('select * from table; --comment', {});
 const s3 = minify('select * from table; --comment', {compress: true});
 
-const options: minify.minifyOptions = {compress: true};
+const options: minify.IMinifyOptions = {compress: true};
 const s4 = minify('select * from table; --comment', options);
